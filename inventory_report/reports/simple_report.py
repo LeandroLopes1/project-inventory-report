ab323d59eab9112d27207e790ab5d9c58987b1d2
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-02-01 18:05:55 -0300

    requisito 01 finalizado

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,29 +1,44 @@
+from datetime import datetime
 
-import datetime
+
+def string_to_datetime(date, format):
+    return datetime.strptime(date, format)
 
 
 def get_oldest_manufacturing_date(stock):
-    fabricacao_mais_antiga = datetime.strptime(
-        stock[0]["data_de_fabricacao"], "%Y-%m-%d"
+    format = "%Y-%m-%d"
+
+    fabricacao_mais_antiga = string_to_datetime(
+        stock[0]["data_de_fabricacao"],
+        format
     )
+
     for item in stock:
-        if datetime.date(item["data_de_fabricacao"]) < fabricacao_mais_antiga:
-            fabricacao_mais_antiga = datetime.date(
-                item["data_de_fabricacao"]
+        if string_to_datetime(item["data_de_fabricacao"], format) < fabricacao_mais_antiga:  # noqa: E501
+            print()
+            fabricacao_mais_antiga = string_to_datetime(
+                item["data_de_fabricacao"],
+                format
             )
-    return fabricacao_mais_antiga
+
+    return fabricacao_mais_antiga.strftime(format)
 
 
 def get_closest_expiration_date(stock):
-    validade_mais_proxima = datetime.date(
-        stock[0]["data_de_validade"]
+    format = "%Y-%m-%d"
+    today = datetime.today()
+    data_de_validade_mais_proxima = string_to_datetime(
+        stock[0]["data_de_validade"],
+        format
     )
     for item in stock:
-        if datetime.date(item["data_de_validade"]) > validade_mais_proxima:
-            validade_mais_proxima = datetime.date(
-                item["data_de_validade"]
-            )
-    return validade_mais_proxima
+        if string_to_datetime(item["data_de_validade"], format) > today:
+            if string_to_datetime(item["data_de_validade"], format) < data_de_validade_mais_proxima:  # noqa: E501
+                data_de_validade_mais_proxima = string_to_datetime(
+                    item["data_de_validade"], format
+                )
+    return data_de_validade_mais_proxima.strftime(format)
+
 
 def get_company_biggest_stock(stock):
     quantidade_de_produto_por_empresa = {}
@@ -32,9 +47,21 @@ def get_company_biggest_stock(stock):
             quantidade_de_produto_por_empresa[item["nome_da_empresa"]] += 1
         else:
             quantidade_de_produto_por_empresa[item["nome_da_empresa"]] = 1
-    print(max(quantidade_de_produto_por_empresa, key=quantidade_de_produto_por_empresa.get))
-    
-    
+    return max(
+        quantidade_de_produto_por_empresa,
+        key=quantidade_de_produto_por_empresa.get
+    )
+
+
+def retornar_report(oldest_date, closest_date, company_name):
+    return (
+        f"Data de fabricação mais antiga: {oldest_date}\n"
+        f"Data de validade mais próxima: {closest_date}\n"
+        "Empresa com maior quantidade de produtos "
+        f"estocados: {company_name}\n"
+    )
+
+
 class SimpleReport:
     def __init__(self):
         self.oldest_manufacturing_date = ''
@@ -44,6 +71,10 @@ class SimpleReport:
     @classmethod
     def generate(self, stock):
         self.oldest_manufacturing_date = get_oldest_manufacturing_date(stock)
-        self.closest_expiration_date = self.get_closest_expiration_date(stock)
-        self.company_biggest_stock = self.get_company_biggest_stock(stock)
-        return self.retornar_report()
+        self.closest_expiration_date = get_closest_expiration_date(stock)
+        self.company_biggest_stock = get_company_biggest_stock(stock)
+        return retornar_report(
+            self.oldest_manufacturing_date,
+            self.closest_expiration_date,
+            self.company_biggest_stock
+        )
