b80b019b99e8a863f6c728cf0db6a63c4108ab5a
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-02-03 15:38:35 -0300

    refactor: ensure class InventoryRefactor implements pattern Interator

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -6,25 +6,21 @@ from inventory_report.reports.complete_report import CompleteReport
 
 
 class InventoryRefactor(Iterable):
-    def __init__(self, collection):
-        # self._read_importer = read_importer
-        self._collection = collection
-        self._position = 0
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
 
     def __iter__(self) -> InventoryIterator:
-        return InventoryIterator(self._collection)
+        return InventoryIterator(self.data)
 
-    @classmethod
     def import_data(self, path_file, method):
-        print(self._collection)
-        # stock_data = self._read_importer.import_data(path_file)
-        # self._collection = stock_data
-        # print(stock_data)
+        stock_data = self.importer.import_data(path_file)
+        self.data = self.data + stock_data
 
-        # method_map = {
-        #     'simples': SimpleReport.generate,
-        #     'completo': CompleteReport.generate
-        # }
+        method_map = {
+            'simples': SimpleReport.generate,
+            'completo': CompleteReport.generate
+        }
 
-        # # print(self.stock_data)
-        # return method_map[method](stock_data)
+        # print(self.stock_data)
+        return method_map[method](stock_data)
